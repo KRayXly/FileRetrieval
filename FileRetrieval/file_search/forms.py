1f06a11707a6e7b8a8f276fff262642f0e70d36b
Jiney88 <2973789947@qq.com>
2023-06-27 22:34:49 +0800

    金凤 2019210308034

--- FileRetrieval/file_search/forms.py
+++ FileRetrieval/file_search/forms.py
@@ -1,12 +1,5 @@
-# from multiupload.fields import MultiFileField
-# from django import forms
-
-
-# class UploadFileForm(forms.Form):
-#     folder = forms.CharField(max_length=255)
-#     files = MultiFileField(min_num=1, max_num=10, max_file_size=1024*1024*5)
-
 from django import forms
 
 class FolderUploadForm(forms.Form):
-    folder = forms.FileField(widget=forms.ClearableFileInput(attrs={'webkitdirectory': True, 'directory': True}))
+    # folder = forms.FileField(widget=forms.ClearableFileInput(attrs={'webkitdirectory': True, 'directory': True}))
+    folder_path = forms.CharField(label='文件夹路径')
--- FileRetrieval/file_search/views.py
+++ FileRetrieval/file_search/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render, redirect
-from .models import UploadFile
-import zipfile
+from .models import Fileinfo
+import os
 from .forms import FolderUploadForm
 
 def file_upload_view(request):
     if request.method == 'POST':
-        form = FolderUploadForm(request.POST, request.FILES)
+        form = FolderUploadForm(request.POST)
         if form.is_valid():
-            folder = form.cleaned_data['folder']
-            files = request.FILES.getlist('files')
-            for file in files:
-                UploadFile.objects.create(folder=folder, file=file)
-            return redirect('file_list')
+            folder_path = form.cleaned_data['folder_path']
+            for root, _, files in os.walk(folder_path):
+                for file_name in files:
+                # 构建完整的文件路径
+                    file_path = os.path.join(root, file_name)
+                # 创建 File 对象并保存到数据库
+                    file = Fileinfo(name=file_name, path=file_path)
+                    file.save()
+        return redirect('file_list')
     else:
         form = FolderUploadForm()
-    context = {'form': form}
-    return render(request, 'upload.html', context)
+    return render(request, 'upload.html', {'form': form})
 
 def file_list_view(request):
-    files = UploadFile.objects.all()
+    files = Fileinfo.objects.all()
     context = {'files': files}
     return render(request, 'file.html', context)
