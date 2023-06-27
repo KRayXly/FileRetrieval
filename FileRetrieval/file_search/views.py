4b29b763ef8515349c8bdd33722ec8b64ca553e9
Xlanalili <741769371@qq.com>
2023-06-28 04:17:35 +0800

    王若澜 2020212205128

--- FileRetrieval/file_search/views.py
+++ FileRetrieval/file_search/views.py
@@ -3,22 +3,30 @@ from .models import Fileinfo
 import os
 from .forms import FolderUploadForm
 
+#设置过滤后的文件类型 当然可以设置多个类型
+filter=[".txt",".doc",".pdf",".docx"]
 def file_upload_view(request):
+    form = FolderUploadForm()
+    files = Fileinfo.objects.all()
+    context = {'files': files,'form': form}
     if request.method == 'POST':
         form = FolderUploadForm(request.POST)
         if form.is_valid():
             folder_path = form.cleaned_data['folder_path']
             for root, _, files in os.walk(folder_path):
                 for file_name in files:
-                # 构建完整的文件路径
+                    # 构建完整的文件路径
                     file_path = os.path.join(root, file_name)
-                # 创建 File 对象并保存到数据库
-                    file = Fileinfo(name=file_name, path=file_path)
-                    file.save()
-        return redirect('file_list')
+                    #获取文件后后缀
+                    ext = os.path.splitext(file_path)[1]
+                    if ext in filter:
+                    # 创建 File 对象并保存到数据库
+                        file = Fileinfo(name=file_name, path=file_path)
+                        file.save()
+        return render(request, 'upload.html',context)
     else:
-        form = FolderUploadForm()
-    return render(request, 'upload.html', {'form': form})
+       Fileinfo.objects.all().delete()
+    return render(request, 'upload.html', context)
 
 def file_list_view(request):
     files = Fileinfo.objects.all()
