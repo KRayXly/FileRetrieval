58b80c39800b7c981fb91f89b6e7e4cdb49dae40
Xlanalili <741769371@qq.com>
2023-06-27 10:49:19 +0800

    王若澜 2020212205128

--- FileRetrieval/FileRetrieval/settings.py
+++ FileRetrieval/FileRetrieval/settings.py
@@ -79,9 +79,16 @@ WSGI_APPLICATION = "FileRetrieval.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',		# 要连接的数据库ip
+        'PORT': '3306',				# 数据库连接端口，一般默认为3306
+        'NAME': 'fileretrieval',	# 已经存在的数据库名称
+        'USER': 'root',				# 用户名
+        'PASSWORD': '123456',		# 用户密码
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            }
     }
 }
 
